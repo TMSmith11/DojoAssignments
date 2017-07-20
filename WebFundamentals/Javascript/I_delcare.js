fd71077de0814e74e5363eebe991fd6f7425493d
Terrance Smith <ohsixgz@yahoo.com>
2017-07-19 22:14:09 -0500

    uploaded i_declare javascript

---
+++ WebFundamentals/Javascript/I_delcare.js
@@ -0,0 +1,27 @@
+var myBirthYear = 1987;
+var sportsNumber = 11;
+var myHeightInCentimeters = 185.42;
+var daysInYear = 365;
+
+var myName = 'Terrance'
+var myTeam = 'Philadelphia Eagles'
+var myDogName = 'Wynnston'
+ 
+var isColdOutside = false;
+var eaglesPlayoffBound = true;
+
+var doIUnderstand = undefined;
+
+function sayHello() {
+    console.log('I was born: ', myBirthYear);
+    console.log('My sports number: ',sportsNumber);
+    console.log('I am this tall: ', myHeightInCentimeters);
+    console.log('Amount of days: ', daysInYear);
+    console.log('I am: ', myName);
+    console.log('Favorite sports team: ', myTeam);
+    console.log('Dog name: ', myDogName);
+    console.log('It is cold outside: ', isColdOutside);
+    console.log('Playoff bound Eagles: ', eaglesPlayoffBound);
+    console.log(doIUnderstand);
+}
+sayHello();
