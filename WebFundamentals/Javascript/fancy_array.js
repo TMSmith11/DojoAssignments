a38de7ac8e869e011151ad2e5527f5b148620d9f
Terrance Smith <ohsixgz@yahoo.com>
2017-07-24 20:04:49 -0500

    added fancy array js assignment

---
+++ WebFundamentals/Javascript/fancy_array.js
@@ -0,0 +1,12 @@
+var arr = ['Hazel', 'Terrance', 'Justin', 'Ty'];
+var Hazel = 0
+var Terrance = 1
+var Justin = 2
+var Ty = 3
+
+
+for (var i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+    
+    
