a3b551049f22cc808bb05f8fb1ca2a684dd2a0ba
Terrance Smith <ohsixgz@yahoo.com>
2017-07-28 10:09:03 -0500

    added students js assignment

---
+++ WebFundamentals/Javascript/students_instructors.js
@@ -0,0 +1,15 @@
+// Create a program that outputs:
+
+// Michael Jordan
+// John Rosales
+// Mark Guillen
+// KB Tonel
+
+
+
+
+
+var array_students = ['Michael Jordan','John Rosales','Mark Guillen','KB Tonel']
+for (var i = 0; i < array_students.length; i++){
+    console.log(array_students[i]);
+}
